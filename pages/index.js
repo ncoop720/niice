11ccf2ff28e9a749244c40931ed86a2e403bc442
unknown <ncoop720@yahoo.com>
2022-01-05 21:40:03 -0500

    next-auth prisma adapter

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,20 @@
+import { useSession, signIn, signOut } from 'next-auth/react'
+
 export default function index() {
-  return <h1>Home Page</h1>
+  const { data } = useSession()
+
+  if (data) {
+    return (
+      <>
+        Signed in as {data.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  )
 }
